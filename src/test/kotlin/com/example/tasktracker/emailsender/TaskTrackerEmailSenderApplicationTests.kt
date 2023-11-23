97e60466c79b6f3bc02f6d4641dccc10a4c8ce03
farneser <farneser1@gmail.com>
2023-11-23 04:08:13 +0300

    init: project files

---
+++ src/test/kotlin/com/example/tasktracker/emailsender/TaskTrackerEmailSenderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tasktracker.emailsender
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskTrackerEmailSenderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
