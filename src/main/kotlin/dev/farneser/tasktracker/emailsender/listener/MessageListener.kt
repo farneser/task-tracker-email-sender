32f6352195bb1c83be91cbea8b5604e10e355575
farneser <farneser1@gmail.com>
2023-11-27 02:52:32 +0300

    feat: scheduled message listener implementation

--- src/main/kotlin/dev/farneser/tasktracker/emailsender/listener/MessageListener.kt
+++ src/main/kotlin/dev/farneser/tasktracker/emailsender/listener/MessageListener.kt
@@ -1,5 +1,9 @@
 package dev.farneser.tasktracker.emailsender.listener
 
+import dev.farneser.tasktracker.emailsender.services.EmailService
+import dev.farneser.tasktracker.emailsender.services.MessageService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
@@ -7,14 +11,29 @@ import org.springframework.stereotype.Component
 
 @Component
 @EnableRabbit
-class MessageListener {
+class MessageListener(private val messageService: MessageService, private val emailService: EmailService) {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(MessageListener::class.java)
+    }
+
     @RabbitListener(queues = [QueueType.NEW_REGISTER])
-    fun receiveMessageFromQueue1(message: String) {
-        println("Received message from register queue: $message")
+    fun receiveMessageFromRegisterQueue(message: String) {
+        log.info("Received message from register queue at ${System.currentTimeMillis()} : $message")
     }
 
     @RabbitListener(queues = [QueueType.SCHEDULED])
-    fun receiveMessageFromQueue2(message: String) {
-        println("Received message from daily queue: $message")
+    fun receiveMessageFromScheduledQueue(message: String) {
+
+        log.info("Received message from scheduled queue at ${System.currentTimeMillis()}: $message")
+
+        val dto = messageService.parseMessage(message)
+
+        log.debug("Parsed message from scheduled queue: {}", dto)
+
+        val result = messageService.buildMessageBody(dto)
+
+        log.info("Sending email to ${dto.email}")
+
+        emailService.send(dto.email, "Scheduled email", result)
     }
 }
