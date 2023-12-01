83c8790e4328e282db95e441fd816649684596e5
farneser <farneser1@gmail.com>
2023-12-01 17:52:03 +0300

    feat: test configuration

---
+++ src/test/kotlin/dev/farneser/tasktracker/emailsender/config/TestContainersConfig.kt
@@ -0,0 +1,46 @@
+package dev.farneser.tasktracker.emailsender.config
+
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.HostConfig
+import com.github.dockerjava.api.model.PortBinding
+import com.github.dockerjava.api.model.Ports
+import com.rabbitmq.client.ConnectionFactory
+import org.springframework.amqp.rabbit.connection.RabbitConnectionFactoryBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.testcontainers.containers.RabbitMQContainer
+
+@Profile("test")
+@Configuration
+class TestContainersConfig {
+    @Bean
+    fun rabbitMqContainer(): ConnectionFactory {
+
+        val container = RabbitMQContainer("rabbitmq:3.8.9-management-alpine")
+            .withUser("rabbitmq", "rabbitmq")
+            .withExposedPorts(5672)
+            .withCreateContainerCmdModifier { cmd ->
+                cmd.withHostConfig(
+                    HostConfig().withPortBindings(
+                        PortBinding(
+                            Ports.Binding.bindPort(15672),
+                            ExposedPort(5672)
+                        )
+                    )
+                )
+            }
+
+        container.start()
+
+        val connectionFactoryBean = RabbitConnectionFactoryBean()
+
+        connectionFactoryBean.setHost(container.host)
+        connectionFactoryBean.setPort(container.amqpPort)
+        connectionFactoryBean.setUsername(container.adminUsername)
+        connectionFactoryBean.setPassword(container.adminPassword)
+        connectionFactoryBean.afterPropertiesSet()
+
+        return connectionFactoryBean.getObject()
+    }
+}
